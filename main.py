906e97cf41c323e60d03fbeb597720afde284788
Pejro-coder <peter.stepanic@hotmail.com>
2024-10-03 15:14:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,55 @@
-##################### Extra Hard Starting Project ######################
-
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
 import datetime as dt
 import pandas
 import random
 import smtplib
 
-# --------------------------- TODAY'S DATE ---------------------------
+# ------------------------------ TODAY'S DATE ------------------------------
 today = dt.datetime.now()
 current_year = today.year
 current_month = today.month
 current_day = today.day
 
-# ---------------------- BIRTHDAY PERSON'S NAME ----------------------
+# ---------------------------- BIRTHDAY NAMES ------------------------------
 data_frame = pandas.read_csv("birthdays.csv")
-print(data_frame)
-filtered_months = data_frame[data_frame.month == 10]
-filtered_days = filtered_months[filtered_months.day == 2]
-filtered_name = filtered_days["name"].to_string()
-filtered_email = filtered_days["email"].to_string()
+filtered_months = data_frame[data_frame.month == current_month]
+filtered_days = filtered_months[filtered_months.day == current_day]
 
-print(filtered_name)
-print(filtered_email)
+filtered_names = []
+filtered_emails = []
 
-
-def clean_string(uncleaned):
-    filtered_string = []
-    allowed_chars = set(" @.")
-    for sign in uncleaned:
-        if sign.isalpha() or sign in allowed_chars:
-            filtered_string.append(sign)
-    return "".join(filtered_string).strip()
+for index, row in filtered_days.iterrows():
+    filtered_names.append(row["name"])
+    filtered_emails.append(row["email"])
 
 
-final_name = clean_string(filtered_name)
-final_email = clean_string(filtered_email)
+print(f"Filtered names: {filtered_names}")
+print(f"Filtered emails: {filtered_emails}")
 
-print()
-print(final_name)
-print(final_email)
-if final_name != "Series":
-# ------------ BIRTHDAY-PERSONS NAME INTO A RANDOM LETTER ------------
+
+if len(filtered_names) > 0:
+    # --------------- BIRTHDAY-PERSONS NAME INTO A RANDOM LETTER ---------------
     random_letter = random.randint(1, 3)
     with open(f"letter_templates/letter_{random_letter}.txt", "r") as letter_file:
-        letter = letter_file.read()
+        letter_template = letter_file.read()
 
-    letter = letter.replace("[NAME]", final_name)
+    final_letters = []
+    for name in filtered_names:
+        letter = letter_template.replace("[NAME]", name)
+        final_letters.append(letter)
 
-    print(letter)
-
-# ------------------------ SEND EMAIL ------------------------
+    # ------------------------------- SEND EMAIL -------------------------------
     MY_GMAIL = "peter.stepanic@gmail.com"
-    MY_PASSWORD = "rduqhmkbprxdhgzk"
+    MY_PASSWORD = "sgfuqhttwlapewzv"
 
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=MY_GMAIL, password=MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_GMAIL,
-            to_addrs=final_email,
-            msg=f"Subject: Good day!\n\n{letter}"
-        )
+    for letter in final_letters:
+        current_email = filtered_emails[final_letters.index(letter)]
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_GMAIL, password=MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_GMAIL,
+                to_addrs=current_email,
+                msg=f"Subject: Happy birthday!\n\n{letter}"
+            )
+        print(f"SENDING EMAIL TO {current_email}")
+
